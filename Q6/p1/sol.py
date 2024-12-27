f06d4f8e84281f2dc7bb1060aee21c3df3383a0f
peras <hugo.s.pereira@tecnico.ulisboa.pt>
2024-12-27 18:46:36 +0000

    Q6 part 1, prty ezz

---
+++ Q6/p1/sol.py
@@ -0,0 +1,37 @@
+graph = {}
+
+with open('../input.txt', 'r') as f:
+    for line in f:
+        node, children = line.strip().split(':')
+        children = children.split(',')
+        graph[node] = children
+
+queue = [("RR", "RR")]
+
+paths = set()
+
+while queue != []:
+    node, path = queue.pop(0)
+
+    if node not in graph:
+        paths.add(path)
+        continue
+
+    for child in graph[node]:
+        queue.append((child, path + child))
+
+answer: dict[str, int] = dict()
+
+for path in paths:
+    length = len(path)
+    if length in answer:
+        answer[length] += 1
+    else:
+        answer[length] = 1
+
+for k, v in answer.items():
+    if v == 1:
+        for path in paths:
+            if len(path) == k:
+                print(path)
+                break
