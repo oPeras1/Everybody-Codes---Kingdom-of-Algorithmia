5e9ff38c4a42e676131f44b27ff104237dabbaac
peras <hugo.s.pereira@tecnico.ulisboa.pt>
2025-01-02 21:25:47 +0000

    Q12, p2. It worked for a bigger input. maybe it works for all?

---
+++ Q12/p2/sol.py
@@ -0,0 +1,51 @@
+targets = []
+
+letters = dict()
+
+maxy = 0
+
+with open("../input.txt") as f:
+    lines = f.readlines()
+    maxy = len(lines)-1
+    for y in range(maxy+1):
+        line = lines[y]
+        for x in range(len(line)):
+            char = line[x]
+            if char == "T" or char == "H":
+                times = 2 if char == "H" else 1
+                targets.append((y,x, times))
+            elif char != "." and char != "=" and char != "\n":
+                letters[char] = (y,x)
+
+count = 0
+
+
+for target in targets:
+    found = False
+    for letter in letters:
+        k = 1
+        yL, xL = letters[letter]
+        yT, xT, times = target
+        xLO = xL
+        while k < abs(xLO-xT):
+            yL, xL = letters[letter]
+            yL -= k
+            xL += 2*k
+            while yL <= maxy:
+                yL += 1
+                xL += 1
+                if (yL,xL) == (yT,xT):
+                    found = True
+                    break
+            if found:
+                break
+            k += 1
+        
+        if found:
+            count += times*(k*(ord(letter)-64))
+            break
+        
+print(count)
+
+            
+
